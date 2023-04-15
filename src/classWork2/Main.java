7210ce2af4e164761423fe07f45a657964e51b56
nicol <nicolfilip48@gmail.com>
2023-04-15 17:53:32 +0300

    added manager

--- src/classWork2/Main.java
+++ src/classWork2/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main (String[] args) {
         new Ligat_HaAl();
         System.out.println("hi nicol");
+        System.out.println("hi manager");
     }
     }
 
